011c4c6c08331bd10ee384592459fc17e609a973
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-14 23:42:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -68,7 +68,7 @@ end
 def add_s(array)
   
  
-  array.each_with_index.collect {|string, element| unless element == 1 string + 's'}
+  array.each_with_index.collect {|string, element| element == 1 ? string : string + 's'}
  
   
 end
